f0e2d661fa5878fcbf58ed22a50a86d4c9bf1d32
unknown <prateekshuklaps0>
2023-03-23 18:55:59 +0530

    raect dom library added.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { createContext } from "react";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<p>Home</p>} />
+        <Route path="/about" element={<p>About</p>} />
+        <Route path="/contact" element={<p>Contact</p>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
